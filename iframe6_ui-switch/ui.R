51f9079db93cf51b336e457da1a9e5ea5d4f2f3a
martinjhnhadley <martinjohnhadley@gmail.com>
2017-07-21 15:12:56 +0100

    Added example without filters

---
+++ iframe6_ui-switch/ui.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(highcharter)
+library(shinyjs)
+library(shinyBS)
+
+shinyUI(
+  navbarPage(
+    useShinyjs(),
+    "Experimental Economics",
+    tabPanel(
+      "Distribution of reports: 6 states",
+      fluidPage(
+        uiOutput("url_allow_popout_UI"),
+        includeCSS("www/animate.min.css"),
+        # provides pulsating effect
+        includeCSS("www/loading-content.css"),
+        div(
+          id = "loading-tab1_6states",
+          class = "loading-content",
+          h2(class = "animated infinite pulse", "Loading data...")
+        ),
+        # highchartOutput("tab1_6states_hc_bubble")
+        uiOutput("tab1_6states_line_hc_UI"),
+        bsModal(
+          "tab1_6states_bubbleModal",
+          "Study Info",
+          trigger = "tab1_6states_line_hc_click",
+          size = "large",
+          uiOutput("tab1_6states_bubble_model_UI")
+        )
+      )
+      
+    ),
+    tabPanel("About",
+             fluidPage(includeMarkdown(
+               knitr::knit("tab_about.Rmd")
+             ))),
+    collapsible = TRUE
+  )
+)
