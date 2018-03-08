c01723f69e278f94476268ffd9e790d216f855ac
martinjhnhadley <martinjohnhadley@gmail.com>
2018-03-08 15:14:37 +0000

    deploy script

---
+++ deployAll.R
@@ -0,0 +1,25 @@
+library(rsconnect)
+
+deployApp("iframe1", launch.browser = FALSE)
+
+deployApp("iframe1_inert", launch.browser = FALSE)
+
+deployApp("iframe2", launch.browser = FALSE)
+
+deployApp("iframe2_inert", launch.browser = FALSE)
+
+deployApp("iframe3", launch.browser = FALSE)
+
+deployApp("iframe3_inert", launch.browser = FALSE)
+
+deployApp("iframe4", launch.browser = FALSE)
+
+deployApp("iframe4_inert", launch.browser = FALSE)
+
+deployApp("iframe5", launch.browser = FALSE)
+
+deployApp("iframe5_inert", launch.browser = FALSE)
+
+deployApp("iframe6", launch.browser = FALSE)
+
+deployApp("iframe6_inert", launch.browser = FALSE)
